fccb4f3ea37d712af5174b62d260cb5c5d93049c
Mirela Loren <20247626+mvlaeva@users.noreply.github.com>
2023-07-21 00:26:01 +0300

    initial config

---
+++ src/main/java/com/example/noservendingmachine/NoserVendingMachineApplication.java
@@ -0,0 +1,13 @@
+package com.example.noservendingmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoserVendingMachineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoserVendingMachineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noservendingmachine/NoserVendingMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noservendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoserVendingMachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
