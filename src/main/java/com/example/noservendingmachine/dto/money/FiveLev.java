e4338b53b99caa0db56077cab49555aba841cdb5
Mirela Loren <20247626+mvlaeva@users.noreply.github.com>
2023-07-21 02:45:12 +0300

    added 5 and 10 levs

---
+++ src/main/java/com/example/noservendingmachine/dto/money/FiveLev.java
@@ -0,0 +1,7 @@
+package com.example.noservendingmachine.dto.money;
+
+public class FiveLev extends Banknote {
+    protected FiveLev() {
+        super(500);
+    }
+}
---
+++ src/main/java/com/example/noservendingmachine/dto/money/TenLev.java
@@ -0,0 +1,7 @@
+package com.example.noservendingmachine.dto.money;
+
+public class TenLev extends Banknote {
+    protected TenLev() {
+        super(1000);
+    }
+}
