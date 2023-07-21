92fb9eb3787e8c169c12700a8e6fdac079ea8dcb
Mirela Loren <20247626+mvlaeva@users.noreply.github.com>
2023-07-21 03:42:07 +0300

    remove boilerplate code, use lombok

--- src/main/java/com/example/noservendingmachine/dto/money/Money.java
+++ src/main/java/com/example/noservendingmachine/dto/money/Money.java
@@ -1,16 +1,13 @@
 package com.example.noservendingmachine.dto.money;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class Money {
     @NotNull
     private final Integer value;
-
-    protected Money(final int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
 }
