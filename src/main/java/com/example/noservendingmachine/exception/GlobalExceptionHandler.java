b94b3b624ffa7b8b836309cde8c889ce3926027b
Mirela Loren <20247626+mvlaeva@users.noreply.github.com>
2023-07-24 04:22:55 +0300

    feat: added swagger

--- src/main/java/com/example/noservendingmachine/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/noservendingmachine/exception/GlobalExceptionHandler.java
@@ -4,15 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BusinessLogicException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleBusinessLogicException(final BusinessLogicException ex) {
         LOGGER.error(ex.getMessage(), ex);
 
@@ -20,6 +22,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(DependencyFailedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleDependencyFailedException(final DependencyFailedException ex) {
         LOGGER.error(ex.getMessage(), ex);
 
@@ -27,6 +30,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MaximumCapacityExceededException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleMaximumCapacityExceededException(final MaximumCapacityExceededException ex) {
         LOGGER.error(ex.getMessage(), ex);
 
@@ -34,6 +38,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MinimumCapacityReached.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<String> handleMinimumCapacityReached(final MinimumCapacityReached ex) {
         LOGGER.error(ex.getMessage(), ex);
 
