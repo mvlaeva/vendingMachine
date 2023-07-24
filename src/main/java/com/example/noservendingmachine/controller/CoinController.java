028887cde24324637ab2385b0fcdb9e644faf9c2
Mirela Loren <20247626+mvlaeva@users.noreply.github.com>
2023-07-24 04:31:21 +0300

    improved swagger documentation

--- src/main/java/com/example/noservendingmachine/controller/CoinController.java
+++ src/main/java/com/example/noservendingmachine/controller/CoinController.java
@@ -3,6 +3,9 @@ package com.example.noservendingmachine.controller;
 import com.example.noservendingmachine.dto.money.Money;
 import com.example.noservendingmachine.exception.DependencyFailedException;
 import com.example.noservendingmachine.service.VendingMachineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,12 +25,21 @@ public class CoinController {
         }
     }
 
+    @Operation(summary = "Insert money in the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Money inserted successfully"),
+            @ApiResponse(responseCode = "400", description = "Validation didn't pass")})
     @PostMapping
     public ResponseEntity<HttpStatus> insertMoney(@Valid @RequestBody final Money money) {
         vendingMachineService.addMoney(money.getValue());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+
+    @Operation(summary = "Return previously inserted money in the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Money returned successfully"),
+            @ApiResponse(responseCode = "400", description = "No money previously inserted")})
     @PutMapping
     public ResponseEntity<Float> returnInsertedMoney() {
         return new ResponseEntity<>(vendingMachineService.returnInsertedMoney(), HttpStatus.OK);
--- src/main/java/com/example/noservendingmachine/controller/ProductController.java
+++ src/main/java/com/example/noservendingmachine/controller/ProductController.java
@@ -3,8 +3,10 @@ package com.example.noservendingmachine.controller;
 import com.example.noservendingmachine.dto.ProductDto;
 import com.example.noservendingmachine.exception.DependencyFailedException;
 import com.example.noservendingmachine.service.VendingMachineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,21 +25,38 @@ public class ProductController {
         }
     }
 
+    @Operation(summary = "Buy a product from the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product bought successfully"),
+            @ApiResponse(responseCode = "400", description = "No such product in the vending machine or insufficient funds")})
     @PutMapping("/buy")
     public ResponseEntity<String> buyProduct(@RequestBody final ProductDto productDto) {
         return new ResponseEntity<>(vendingMachineService.buyProduct(productDto), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add a product in the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product added successfully"),
+            @ApiResponse(responseCode = "400", description = "Maximum capacity exceeded")})
     @PostMapping
     public ResponseEntity<ProductDto> addProduct(@Valid @RequestBody final ProductDto productDto) {
         return new ResponseEntity<>(vendingMachineService.addProduct(productDto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update a product in the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product updated successfully"),
+            @ApiResponse(responseCode = "400", description = "No such product in the vending machine")})
     @PutMapping
     public ResponseEntity<ProductDto> updateProduct(@Valid @RequestBody final ProductDto productDto) {
         return new ResponseEntity<>(vendingMachineService.updateProduct(productDto), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Delete a product from the vending machine")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "No such product in the vending machine")})
     @DeleteMapping("/{productId}")
     public ResponseEntity<Long> deleteProduct(@PathVariable final Long productId) {
         var isDeleted = vendingMachineService.deleteProduct(productId);
